4cb44570ea0bc58d5c14be2a689844f3835e8e2f
Hamza Tariq <hamzabtariq2@gmail.com>
2022-10-16 08:38:13 +0500

    uploading data and data collection script

--- data_collection.py
+++ data_collection.py
@@ -9,5 +9,7 @@ import glassdoor_scraper as gs
 import pandas as pd
 path = "E:/DataScience_Projects/ds_salary_proj/chromedriver"
 
-df = gs.get_jobs('data scientist', 15, False, path, 15)
+df = gs.get_jobs('data scientist',1000, False, path, 15)
+
+df.to_csv('glassdoor_jobs.csv', index = False)
 
