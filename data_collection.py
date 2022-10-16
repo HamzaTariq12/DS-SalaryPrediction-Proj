7fa50e8a452103b75b740c79b6f684f7d65c297d
Hamza Tariq <hamzabtariq2@gmail.com>
2022-10-16 08:14:49 +0500

    data scrapped from glass door using selenium

---
+++ data_collection.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 15 12:23:18 2022
+
+@author: Negative
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+path = "E:/DataScience_Projects/ds_salary_proj/chromedriver"
+
+df = gs.get_jobs('data scientist', 15, False, path, 15)
+
